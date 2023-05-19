3f075034779e947875e54db37b4ca2e46a72eb54
disaacjm <disaacjm@gmail.com>
2023-05-19 11:02:06 +0100

    refactor: create game class

--- js/main.js
+++ js/main.js
@@ -1,3 +1,73 @@
+class Game {
+  constructor() {
+    this.player = null;
+    this.obstaclesArr = []; // will store instances of the class Obstacle
+  }
+  start() {
+    this.player = new Player();
+
+    this.attachEventListeners();
+
+    // Create new obstacles
+    setInterval(() => {
+      const newObstacle = new Obstacle();
+      this.obstaclesArr.push(newObstacle);
+    }, 4000);
+
+    // Update obstacles
+    setInterval(() => {
+      this.obstaclesArr.forEach((obstacleInstance) => {
+        // Move current obstacle
+        obstacleInstance.moveDown();
+
+        // Detect collision
+        this.detectCollision(obstacleInstance);
+
+        // Detect if obstacle needs to be removed
+        this.removeObstacleIfOutside(obstacleInstance);
+      });
+    }, 60);
+  }
+
+  attachEventListeners() {
+    document.addEventListener("keydown", (event) => {
+      if (event.code === "ArrowLeft") {
+        this.player.moveLeft();
+      } else if (event.code === "ArrowRight") {
+        this.player.moveRight();
+      } else if (event.code === "ArrowUp") {
+        this.player.moveUp();
+      } else if (event.code === "ArrowDown") {
+        this.player.moveDown();
+      }
+    });
+  }
+
+  detectCollision(obstacleInstance) {
+    if (
+      obstacleInstance.positionX < this.player.positionX + this.player.width &&
+      obstacleInstance.positionX + obstacleInstance.width >
+        this.player.positionX &&
+      obstacleInstance.positionY < this.player.positionY + this.player.height &&
+      obstacleInstance.height + obstacleInstance.positionY >
+        this.player.positionY
+    ) {
+      console.log("game over my fren");
+      location.href = "./gameover.html";
+    }
+  }
+
+  removeObstacleIfOutside(obstacleInstance) {
+    if (obstacleInstance.positionY < 0 - obstacleInstance.height) {
+      //1. remove elm from the dom
+      obstacleInstance.domElement.remove();
+
+      //2. remove from the array of obstacles
+      this.obstaclesArr.shift();
+    }
+  }
+}
+
 class Player {
   constructor() {
     this.width = 20;
@@ -5,17 +75,17 @@ class Player {
     this.positionX = 50 - this.width / 2;
     this.positionY = 0;
 
-    this.domElement = null;
+    this.domElement = null; // we will store a ref. to the dom element of the player
 
     this.createDomElement();
   }
 
   createDomElement() {
+    // step1: create the element
     this.domElement = document.createElement("div");
 
     // step2: add content or modify (ex. innerHTML...)
     this.domElement.id = "player";
-    this.domElement.innerText = "This is the player";
     this.domElement.style.width = this.width + "vw";
     this.domElement.style.height = this.height + "vh";
     this.domElement.style.left = this.positionX + "vw";
@@ -27,25 +97,20 @@ class Player {
   }
 
   moveLeft() {
-    this.positionX--;
-    this.domElement.style.left = this.positionX + "vw";
-    console.log(`new position.... ${this.positionX}`);
+    this.positionX--; //modify the position
+    this.domElement.style.left = this.positionX + "vw"; //reflect change in the css
   }
   moveRight() {
-    this.positionX++;
-    this.domElement.style.left = this.positionX + "vw";
-    console.log(`new position.... ${this.positionX}`);
+    this.positionX++; //modify the position
+    this.domElement.style.left = this.positionX + "vw"; //reflect change in the css
   }
-
   moveUp() {
-    this.positionY++;
-    this.domElement.style.bottom = this.positionY + "vh";
-    console.log(`new position.... ${this.positionY}`);
+    this.positionY++; //modify the position
+    this.domElement.style.bottom = this.positionY + "vh"; //reflect change in the css
   }
   moveDown() {
-    this.positionY--;
-    this.domElement.style.bottom = this.positionY + "vh";
-    console.log(`new position.... ${this.positionY}`);
+    this.positionY--; //modify the position
+    this.domElement.style.bottom = this.positionY + "vh"; //reflect change in the css
   }
 }
 
@@ -55,6 +120,7 @@ class Obstacle {
     this.height = 10;
     this.positionX = 50 - this.width / 2;
     this.positionY = 100;
+
     this.domElement = null;
 
     this.createDomElement();
@@ -80,52 +146,5 @@ class Obstacle {
   }
 }
 
-const player = new Player();
-const obstaclesArr = [];
-
-// Create new obstacles
-setInterval(() => {
-  const newObstacle = new Obstacle();
-  obstaclesArr.push(newObstacle);
-}, 4000);
-
-// Move all obstacles
-setInterval(() => {
-  obstaclesArr.forEach((obstacleInstance) => {
-    // Move current obstacle
-    obstacleInstance.moveDown();
-
-    // Detect collision
-    if (
-      obstacleInstance.positionX < player.positionX + player.width &&
-      obstacleInstance.positionX + obstacleInstance.width > player.positionX &&
-      obstacleInstance.positionY < player.positionY + player.height &&
-      obstacleInstance.height + obstacleInstance.positionY > player.positionY
-    ) {
-      console.log("game over my fren");
-      location.href = "./gameover.html";
-    }
-
-    // Detect if obstacle needs to be removed
-    if (obstacleInstance.positionY < 0 - obstacleInstance.height) {
-      //1. remove elm from the dom
-      obstacleInstance.domElement.remove();
-
-      //2. remove from the array of obstacles
-      obstaclesArr.shift(); //remove from the array
-    }
-  });
-}, 60);
-
-// attach event listeners...
-document.addEventListener("keydown", (event) => {
-  if (event.code === "ArrowLeft") {
-    player.moveLeft();
-  } else if (event.code === "ArrowRight") {
-    player.moveRight();
-  } else if (event.code === "ArrowUp") {
-    player.moveUp();
-  } else if (event.code === "ArrowDown") {
-    player.moveDown();
-  }
-});
+const game = new Game();
+game.start();
